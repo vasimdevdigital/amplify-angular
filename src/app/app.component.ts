52379753dffe009c8b1d443d84063e1f8fe72143
vasimdevdigital <vasim.shaikh@devdigital.com>
2021-03-30 21:59:15 +0530

    Add  Angular CognitoAuth UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { onAuthUIStateChange, CognitoUserInterface, AuthState } from '@aws-amplify/ui-components';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'amplifycrud';
+  user: CognitoUserInterface | undefined;
+  authState: AuthState | undefined;
+  formFields = [
+    {
+      type: "email",
+      label: "Custom email Label",
+      placeholder: "custom email placeholder",
+      required: true,
+    },
+    {
+      type: "password",
+      label: "Custom Password Label",
+      placeholder: "custom password placeholder",
+      required: true,
+    },
+    {
+      type: "phone_number",
+      label: "Custom Phone Label",
+      placeholder: "custom Phone placeholder",
+      required: false,
+    },
+  ];
+  constructor(private ref: ChangeDetectorRef) {}
+  
+  ngOnInit(){
+    onAuthUIStateChange((authState, authData)=>{
+      this.authState = authState;
+      this.user = authData as CognitoUserInterface;
+      console.log(this.user);
+      this.ref.detectChanges();
+    })
+  }
+
+  ngOnDestroy() {
+    return onAuthUIStateChange;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { Amplify } from '@aws-amplify/core';
+import { Auth } from '@aws-amplify/auth';
+import awsconfig from './aws-exports';
+
 if (environment.production) {
   enableProdMode();
 }
+
+Amplify.configure(awsconfig);
+Auth.configure(awsconfig);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
